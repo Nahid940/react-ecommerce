a473166f088d00fc28b83df7450c58d5c656049d
nahid940 <nahid940@gmail.com>
2020-03-15 23:47:42 +0600

    Changes added(Action types seperated)

--- src/redux/cart/cart-reducer.jsx
+++ src/redux/cart/cart-reducer.jsx
@@ -1,4 +1,5 @@
 import {addItemToCart} from './cart.utils'
+import {cartActionType} from './actions'
 const INITIAL_STATE={
     cartItems:[],
 }
@@ -6,7 +7,7 @@ const INITIAL_STATE={
 const cartReducer=(state=INITIAL_STATE,action)=>{
     switch(action.type)
     {
-        case 'ADD_TO_CART':
+        case cartActionType.ADD_TO_CART:
             return{
                 ...state,
                 cartItems:addItemToCart(state.cartItems,action.payload)
